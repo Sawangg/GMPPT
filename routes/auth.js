f8cd003ab4a3714d4dc1d0c635efab88c6f6adf2
Sawangg <38500427+Sawangg@users.noreply.github.com>
2020-12-04 15:27:08 +0100

    Ajout route auth par défaut

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,14 @@ const { Router } = require("express");
 const passport = require("passport");
 const router = Router();
 
+router.get("/", (req, res) => {
+    if(req.user) {
+        res.send(req.user);
+    } else {
+        res.status(401).send({ msg: "Unauthorized" });
+    }
+});
+
 router.post('/login', passport.authenticate('local'), (req, res) => {
     res.sendStatus(200);
 });
