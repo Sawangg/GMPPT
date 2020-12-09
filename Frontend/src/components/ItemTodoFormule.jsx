4a5bbc0f0c083c3b4905939067a13f80fa004dd9
Florian Toribio <58748740+sepios87@users.noreply.github.com>
2020-12-09 21:40:14 +0100

    deplacement items fromules

--- Frontend/src/components/ItemTodoFormule.jsx
+++ Frontend/src/components/ItemTodoFormule.jsx
@@ -2,34 +2,40 @@ import React from 'react';
 import { Button, TextField, Typography, Fab } from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import DeleteIcon from '@material-ui/icons/Delete';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 import '../styles/ItemTodoFormule.css'
 
 export default function Item(props) {
 
     return (
-        <div className="container" key={props.item.index}>
+        <div className="container">
+            
+            <Fab className="center" size="small" color="secondary" aria-label="add" onClick={e => props.removeTodo()}><DeleteIcon className="center" /></Fab>
             
             {props.item.modif ? <>
-
-                    <TextField className="center fieldFormule" multiline label="Nom formule" variant="outlined" size="small" value={props.item.nomFormule} onChange={e => props.onChange(e, undefined)} />
+                <div className="affichageFormule">
+                    <TextField className="nomFormule center" multiline label="Nom formule" variant="outlined" size="small" value={props.item.nomFormule} onChange={e => props.onChange(e, undefined)} />
                     <ArrowForwardIcon className="center" />
-                    <TextField className="center" multiline label="formule" variant="outlined" size="small" value={props.item.formule} onChange={e => props.onChange(undefined, e)}/>
+                    <TextField className="formule center" multiline label="formule" variant="outlined" size="small" value={props.item.formule} onChange={e => props.onChange(undefined, e)}/>
+                </div>
                     <Button className="buttonItem center ButtonEnregistrer" variant="contained" onClick={e => props.changeModif()}>Enregistrer</Button>
             
             </> : <>
 
-                    <Typography className="typoFormule">{props.item.nomFormule}</Typography>
+            <div className="affichageFormule">
+                    <Typography className="nomFormule typoFormule center">{props.item.nomFormule}</Typography>
                     <ArrowForwardIcon className="center" />
-                    <Typography className="typoFormule">{props.item.formule}</Typography>
+                    <Typography className="typoFormule formule center">{props.item.formule}</Typography>
+                </div>
                     <Button className="buttonItem center" variant="contained" onClick={e => props.changeModif()}>Modifier</Button>
                
             </>}
-
-                <Fab className="center" size="small" color="secondary" aria-label="add" onClick={e => props.removeTodo()}>
-                    <DeleteIcon className="center" />
-                </Fab>
-
+                <div className="center" style={{width : "15%"}}>
+                    <Fab color="primary" variant='extended' size='small' onClick={e => props.changePosition(true)}><ArrowUpwardIcon/></Fab>
+                    <Fab style={{marginLeft : "10%"}} color="primary" variant='extended' size='small' onClick={e => props.changePosition(false)}><ArrowDownwardIcon/></Fab>
+                </div>
         </div>
     )
     
