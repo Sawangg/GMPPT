fc74798d2aed2f6d41deef021289ad15d1a5c1ee
Florian Toribio <58748740+sepios87@users.noreply.github.com>
2020-12-16 18:13:50 +0100

    categorie sync

--- Frontend/src/pages/Formules.jsx
+++ Frontend/src/pages/Formules.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Fab, Button, CircularProgress} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 import Items from '../components/ItemTodoAccordeon'
 import useConstructor from '../components/useContructor'
 
-import { formules } from '../utils/api.js';
+import { formules, getCategories } from '../utils/api.js';
 
 import { useDispatch } from "react-redux";
-import { addCategorie } from "../slice/FormulesSlice";
+import { addCategorie, setTab } from "../slice/FormulesSlice";
 import { useSelector } from "react-redux";
 import { selectFormule } from "../slice/FormulesSlice"
 
@@ -16,22 +16,17 @@ export default function TodoListAccordeon() {
 
     const dispatch = useDispatch();
     const tab = useSelector(selectFormule);
+    
+    const [chargement, setChargement] =  useState(true)
 
-    // useConstructor(() => {
-    //     getCategories()
-    //     .then((data) => {
-    //         let newArray = [];
-    //         if (data.data.length === 0){
-    //             newArray.push({nom : "", modif : true, index : 0, margeErreur : "5", tabFormule : []})
-    //         } else {
-    //         data.data.forEach(element => 
-    //             newArray.push({nom : element.nom, modif : false, index : element.idx, margeErreur : element.margeErreur, tabFormule : []})
-    //             );
-    //         }
-    //         setTab(newArray)
-    //     })
-    //     .catch(() => setTab([{nom : "", modif : true, index : 0, margeErreur : "5", tabFormule : []}]));
-    // });
+    useConstructor(() => {
+        getCategories()
+        .then((data) => {
+            dispatch(setTab(data.data));
+            setChargement(false);
+        })
+        .catch(() => null);
+    });
 
     const addValue = () => {
         dispatch(addCategorie());
@@ -40,6 +35,8 @@ export default function TodoListAccordeon() {
     return (
         <div>
             <h1 style={{textAlign : "center"}}>Creation des formules</h1>
+            {chargement ? <CircularProgress className="center"/> :
+            <>
             <Fab style={{marginLeft : "5%"}} size="small" color="primary" aria-label="add" onClick={(e => addValue())}>
                 <AddIcon />
             </Fab>
@@ -47,6 +44,7 @@ export default function TodoListAccordeon() {
             {tab.map((i, id) => (
             <Items index={id} key={i.index} item={i} length={tab.length}/>
             ))}
+            </>}
         </div>
     );
 }
