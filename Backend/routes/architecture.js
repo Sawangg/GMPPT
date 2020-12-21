19493b6b20d56f0fbf8c38527effdf215c562558
Gauthier Raphael <raphael.gauthier@etu.unilim.fr>
2020-12-21 17:49:34 +0100

    correction code err

--- Backend/routes/architecture.js
+++ Backend/routes/architecture.js
@@ -21,11 +21,13 @@ router.get('/:idarchi/modeles', isAuthenticated, isProf, async (req, res) => {
     const { idarchi } = req.params;
     db.promise().query(`SELECT * FROM modeles3D WHERE id_architecture=${idarchi}`)
     .then(([rows], err) => {
-        if (err || rows.length == 0) {
+        if(!rows[0]) return res.sendStatus(404);
+        if (err) {
             res.sendStatus(403);
         } else {
             res.status(200).send(rows);
         }
     });
 });
+
 module.exports = router;
