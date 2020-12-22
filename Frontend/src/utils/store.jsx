6beb2c5ea4d4546417037bd7f9ef5e1f829cf81e
Florian Toribio <58748740+sepios87@users.noreply.github.com>
2020-12-22 13:03:43 +0100

    drop file + modele

--- Frontend/src/utils/store.jsx
+++ Frontend/src/utils/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../slice/UserSlice';
 import formuleReducer from '../slice/FormulesSlice';
+import modeleReducer from '../slice/ModeleSlice';
 
 export default configureStore({
   reducer: {
     user: userReducer,
     formule: formuleReducer,
+    modele: modeleReducer,
   },
 });
