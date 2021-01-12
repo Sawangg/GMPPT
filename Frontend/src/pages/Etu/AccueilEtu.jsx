cb0191927b3ff735dd4e46428b97736e09af9e83
lrenon <leanar@live.fr>
2021-01-12 16:06:21 +0100

    modif style accueil etu ok

--- Frontend/src/pages/Etu/AccueilEtu.jsx
+++ Frontend/src/pages/Etu/AccueilEtu.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 import MenuProfil from '../../components/MenuProfil'
+import {makeStyles} from "@material-ui/core";
 
 
 export default function Accueil(props) {
 
+    const useStyles = makeStyles((theme) => ({
+        divProgress: {
+            width : "50%",
+            margin : "auto"
+        }
+    }));
+    const classes = useStyles();
+
     return (
         <div>
             <MenuProfil info={props.info} />
-            <div style={{width : "50%", margin : "auto"}}>
+            <div className={classes.divProgress}>
                 <p>Progression dans le traitement du sujet</p>
                 <LinearProgress  variant="determinate" value={10}/>
                 <p>Date limite de rendu : 10/01/21</p>
