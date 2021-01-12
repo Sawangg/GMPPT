b83747638cde8709ab96baaf0bffa61c95148dc8
lrenon <leanar@live.fr>
2021-01-12 14:41:29 +0100

    modifs style accueil prof ok

--- Frontend/src/pages/Prof/AccueilProf.jsx
+++ Frontend/src/pages/Prof/AccueilProf.jsx
@@ -3,16 +3,27 @@ import React, {useState} from 'react'
 import Etapes from '../../components/Etapes';
 import ParticulesFond from '../../components/ParticulesFond';
 import GestionUnite from '../../components/GestionUnite'
+import {makeStyles} from "@material-ui/core";
 
 export default function Accueil() {
 
+    const useStyles = makeStyles((theme) => ({
+        button: {
+            position : "relative",
+            zIndex : 100,
+            display: "block",
+            margin: "auto"
+        }
+    }));
+
+    const classes = useStyles();
     const [open, setOpen] = useState(false);
 
     return (
         <div>
             <ParticulesFond/>
             <Etapes/>
-            <button className="center" style={{position : "relative", zIndex : 100}} onClick={() => setOpen(true)}>ici</button>
+            <button className={classes.button} onClick={() => setOpen(true)}>ici</button>
             <GestionUnite tard={true} setClose={() => setOpen(false)} open={open}/>
         </div>
     );
