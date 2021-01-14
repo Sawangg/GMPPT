c7d2bdf4fcc155b7944255b85849fc9d11b33983
Sawangg <38500427+Sawangg@users.noreply.github.com>
2021-01-14 10:27:15 +0100

    Ajout de l'importation des architectures + refactoring des unites

--- Backend/routes/architecture.js
+++ Backend/routes/architecture.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const db = require("../databases.js");
 const router = Router();
+const ExcelJS = require('exceljs');
 const { isAuthenticated, isProf } = require("../middleware.js");
 
 router.post('/:id_architecture/modeles/new', isAuthenticated, isProf, async (req, res) => {
@@ -25,6 +26,22 @@ router.get('/:id_architecture/modeles', isAuthenticated, isProf, (req, res) => {
     }).catch(() => {
         return res.sendStatus(500);
     });
+});
+
+router.post('/new', isAuthenticated, isProf, async (req, res) => {
+    try {
+        const workbook = new ExcelJS.Workbook();
+        const feuille = (await workbook.xlsx.load(req.files.fileUploaded.data)).worksheets[0];
+        await db.promise().execute(`DELETE FROM architecture`);
+        let insertArchi = 'INSERT INTO architecture VALUES ';
+        feuille.eachRow((row, number) => {
+            insertArchi += ` ('${number}', ${row.getCell(1).value}, ${row.getCell(2).value}, ${row.getCell(3).value}, ${row.getCell(4).value}, ${row.getCell(5).value}, ${row.getCell(6).value}, ${row.getCell(7).value}, ${row.getCell(8).value}, ${row.getCell(9).value}),`;
+        });
+        await db.promise().execute(insertArchi);
+        return res.sendStatus(200);
+    } catch {
+        return res.sendStatus(500);
+    }
 });
 
 module.exports = router;
--- Backend/routes/unite.js
+++ Backend/routes/unite.js
@@ -5,7 +5,7 @@ const { isAuthenticated, isProf } = require("../middleware.js");
 
 router.post('/new', isAuthenticated, isProf, async (req, res) => {
     const { nom, abrev } = req.body;
-    db.promise().execute(`INSERT INTO unite VALUES (NULL,'${nom}', '${abrev}')`).then(([rows]) => {
+    db.promise().execute(`INSERT INTO unite VALUES ('${nom}', '${abrev}')`).then(([rows]) => {
         return res.send(rows).status(200);
     }).catch(() => {
         return res.sendStatus(500);
@@ -20,9 +20,9 @@ router.get('/', isAuthenticated, isProf, async (_req, res) => {
     });
 });
 
-router.get('/:idunite/delete', isAuthenticated, isProf, async (req, res) => {
-    const { idunite } = req.params;
-    db.promise().execute(`DELETE FROM unite WHERE id_unite=${idunite}`).then(([rows]) => {
+router.get('/:nom/delete', isAuthenticated, isProf, async (req, res) => {
+    const { nom } = req.params;
+    db.promise().execute(`DELETE FROM unite WHERE nom = ${nom}`).then(([rows]) => {
         return res.send(rows).status(200);
     }).catch(() => {
         return res.sendStatus(500);
