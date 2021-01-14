4c36cc98cdeac5086e14680ea1cded2f1d30599c
Sawangg <38500427+Sawangg@users.noreply.github.com>
2021-01-14 09:35:12 +0100

    Changement stratégie pour obtenir id_promo dans /auth/

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -7,7 +7,7 @@ const { comparePwd, encrypt } = require("../utils.js");
 
 
 router.get("/", isAuthenticated, (req, res) => {
-    return res.send({username : req.user.username, isProf : req.user.isProf}).status(200);
+    return res.send({username : req.user.username, isProf : req.user.isProf, idPromo : req.user.id_promo}).status(200);
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
