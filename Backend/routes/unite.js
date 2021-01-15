a2ba4775c32ac63880fad6c6bdf11aab6524eef9
Sawangg <38500427+Sawangg@users.noreply.github.com>
2021-01-15 10:12:52 +0100

    Changement route unité et refactoring /reponses/new & fix RépondreQuestionsSlice

--- Backend/routes/unite.js
+++ Backend/routes/unite.js
@@ -4,25 +4,21 @@ const router = Router();
 const { isAuthenticated, isProf } = require("../middleware.js");
 
 router.post('/new', isAuthenticated, isProf, async (req, res) => {
-    const { nom, abrev } = req.body;
-    db.promise().execute(`INSERT INTO unite VALUES ('${nom}', '${abrev}')`).then(([rows]) => {
-        return res.send(rows).status(200);
-    }).catch(() => {
+    let insert = 'INSERT INTO unite VALUES';
+    try {
+        req.body.tabUnites.forEach(unite => {
+            insert += ` ('${unite.nom}', '${unite.abrev}'),`;
+        });
+        insert = insert.slice(0, -1);
+        await db.promise().execute(insert);
+        return res.sendStatus(200);
+    } catch {
         return res.sendStatus(500);
-    });
+    }
 });
 
 router.get('/', isAuthenticated, async (_req, res) => {
     db.promise().execute(`SELECT * FROM unite`).then(([rows]) => {
-        return res.send(rows).status(200);
-    }).catch(() => {
-        return res.sendStatus(500);
-    });
-});
-
-router.get('/:nom/delete', isAuthenticated, isProf, async (req, res) => {
-    const { nom } = req.params;
-    db.promise().execute(`DELETE FROM unite WHERE nom = ${nom}`).then(([rows]) => {
         return res.send(rows).status(200);
     }).catch(() => {
         return res.sendStatus(500);
