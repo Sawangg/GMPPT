c6c03e3e92e24af82c64595fac048e0c7647216d
Sylvain <sylvain.frediani@etu.unilim.fr>
2021-01-21 14:54:25 +0100

    table pour consulter les étudiants reliée à l'API

--- Frontend/src/slice/CorrectionSlice.jsx
+++ Frontend/src/slice/CorrectionSlice.jsx
@@ -1,38 +1,67 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getInfoPromoAPI} from "../utils/api.js";
+
+export const getEtudiantsDB = createAsyncThunk("promo/getEtudiants", 
+async (idPromo) => {
+    const response = await getInfoPromoAPI(idPromo);
+    return response.data;
+});
 
 export const correctionSlice = createSlice({
     name: 'correction',
     initialState: {
-        tabEtudiants : [{
-            id : 0,
-            nom : 'undefined',
-            prenom : '',
-            promo : '',
-            avancement : '', // %
-            note : '' // /20
-        }],
+        tabEtudiants : [],
+        idPromo : undefined,
+        // tabEtudiants : [{
+        //     id : 0,
+        //     nom : 'undefined',
+        //     prenom : '',
+        //     promo : '',
+        //     avancement : '', // %
+        //     note : '' // /20
+        // }],
     },
     reducers: {
         //importe un jeu d'essai pour faire des tests sur liste d'étudiants
         setEtudiantsForTests : (state) =>{
             state.tabEtudiants = [
-                {id : 0, prenom : 'Florian', nom : 'TORIBIO', promo : 'Année 2', avancement : 10, note : 4},
-                {id : 1, prenom : 'Raphael', nom : 'GAUTHIER', promo : 'Année 2', avancement : 80, note : 17},
-                {id : 2, prenom : 'Léana', nom :'RENON', promo : 'Année 2', avancement : 50, note : 16},
-                {id : 5, prenom : 'Léo', nom : 'MERCIER', promo : 'Année 2', avancement : 40, note : 11},
-                {id : 9, prenom : 'Sylvain', nom : 'FREDIANI', promo : 'Année 2', avancement : 90, note : 16},
-                {id : 15, prenom : 'Test', nom : 'TEST', promo : 'Année 1', avancement : 100, note :  20},
+                {id : 0, prenom : 'Florian', nom : 'TORIBIO', avancement : 10, note : 4},
+                {id : 1, prenom : 'Raphael', nom : 'GAUTHIER', avancement : 80, note : 17},
+                {id : 2, prenom : 'Léana', nom :'RENON', avancement : 50, note : 16},
+                {id : 5, prenom : 'Léo', nom : 'MERCIER', avancement : 40, note : 11},
+                {id : 9, prenom : 'Sylvain', nom : 'FREDIANI', avancement : 90, note : 16},
+                {id : 15, prenom : 'Test', nom : 'TEST', avancement : 100, note :  20},
             ]
         },
+        //change l'id de la promo que le prof est en train de corriger
+        setIdPromo : (state, action) =>{
+            state.idPromo = action.payload
+        }
 
     },
-    extraReducers: {}
+    extraReducers: {
+        [getEtudiantsDB.fulfilled] : (state, action) => {
+            state.tabEtudiants = [];
+            action.payload.forEach(etudiant => {
+                state.tabEtudiants.push({
+                    id : etudiant.id_auth,
+                    prenom : etudiant.prenom,
+                    nom : etudiant.nom,
+                    avancement : 50,
+                    note : 16
+                })
+            });
+        }
+    }
 })
 
 
-export const { setEtudiantsForTests } = correctionSlice.actions
+export const { setEtudiantsForTests, setIdPromo } = correctionSlice.actions
 
 //retourne tous le tableau d'étudiants
 export const selectEtudiants = state => state.correction.tabEtudiants
+
+//retourne l'id de promo qui est en train de se faire corriger
+export const selectIdPromo = state => state.correction.idPromo
 
 export default correctionSlice.reducer;
