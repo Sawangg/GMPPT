83cff8f8077be86c71d8165e19d9c35d48706183
Florian Toribio <58748740+sepios87@users.noreply.github.com>
2021-01-22 20:15:25 +0100

    améliorations modele + input await + argumentation

--- Frontend/src/components/InputAwait.jsx
+++ Frontend/src/components/InputAwait.jsx
@@ -2,7 +2,7 @@ import React, {useRef, useState} from 'react';
 import { TextField } from '@material-ui/core';
 import _ from "lodash";
 
-export default function DebounceInput({label, typeNumber, onChange, delay, value}) {
+export default function DebounceInput({label, typeNumber, onChange, delay, value, style, rows}) {
   
     const [userdata, setUserdata] = useState(value);
 
@@ -17,7 +17,9 @@ export default function DebounceInput({label, typeNumber, onChange, delay, value
 
     return (
       <TextField 
+          style={style}
           multiline 
+          rows={rows}
           variant="outlined" 
           size="small" 
           label={label} 
