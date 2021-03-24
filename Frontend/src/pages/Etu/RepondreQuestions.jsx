53a2a23446d07396057f07f4607d1178a8eb0874
Sawangg <38500427+Sawangg@users.noreply.github.com>
2021-03-24 10:40:56 +0100

    Fix warnings

--- Frontend/src/pages/Etu/RepondreQuestions.jsx
+++ Frontend/src/pages/Etu/RepondreQuestions.jsx
@@ -1,9 +1,9 @@
 import React, { useCallback } from 'react';
 import ReactHtmlParser from 'react-html-parser';
-import jsPDF from 'jspdf';
+// import jsPDF from 'jspdf';
 import { Button, makeStyles, Typography } from '@material-ui/core';
 
-import GetAppIcon from '@material-ui/icons/GetApp';
+// import GetAppIcon from '@material-ui/icons/GetApp';
 
 import { useDispatch, useSelector } from "react-redux";
 import { selectReponses, getSujet, enregistrerReponses, selectSujetEnregistre, etudiantVariables, getModele3D, getEtudiantModele } from "../../slice/RepondreQuestionsSlice";
@@ -70,57 +70,57 @@ export default function RepondreQuestions() {
     }, [dispatch, reponses]);
 
     //trandforme en pdf le sujet
-    const downloadPdf = () => {
-        const MARGE_COTE = 15;
-        const MARGE_HAUT = 20;
-        const MARGE_BAS = 20;
-        const HAUTEUR_A4 = 297;
-        const LARGEUR_A4 = 210;
+    // const downloadPdf = () => {
+    //     const MARGE_COTE = 15;
+    //     const MARGE_HAUT = 20;
+    //     const MARGE_BAS = 20;
+    //     const HAUTEUR_A4 = 297;
+    //     const LARGEUR_A4 = 210;
 
-        //met le sujet dans la bonne font family
-        let sujetForPdf = '<p style="font-family: sans-serif; display : block; margin : auto;">' + reponses.sujet + '</p><br/><br/><br/><br/><hr>'
+    //     //met le sujet dans la bonne font family
+    //     let sujetForPdf = '<p style="font-family: sans-serif; display : block; margin : auto;">' + reponses.sujet + '</p><br/><br/><br/><br/><hr>'
 
-        reponses.tabQuestions.forEach((elem, index) => {
-            sujetForPdf += `<p>Question ${index + 1} :</p><br/>${elem.enonce}<br/><br/>`;
-        });
+    //     reponses.tabQuestions.forEach((elem, index) => {
+    //         sujetForPdf += `<p>Question ${index + 1} :</p><br/>${elem.enonce}<br/><br/>`;
+    //     });
 
-        //document pdf en format a4
-        const doc = new jsPDF('p', 'mm', 'a4');
+    //     //document pdf en format a4
+    //     const doc = new jsPDF('p', 'mm', 'a4');
 
-        const options = {
-            pagesplit: true,
-            'width': LARGEUR_A4 - 2 * MARGE_COTE,
-            'height': HAUTEUR_A4 - MARGE_HAUT - MARGE_BAS,
-        };
+    //     const options = {
+    //         pagesplit: true,
+    //         'width': LARGEUR_A4 - 2 * MARGE_COTE,
+    //         'height': HAUTEUR_A4 - MARGE_HAUT - MARGE_BAS,
+    //     };
 
-        //transmet le sujet au document pdf
-        doc.fromHTML(sujetForPdf, MARGE_COTE, MARGE_HAUT + 10, options);
+    //     //transmet le sujet au document pdf
+    //     doc.fromHTML(sujetForPdf, MARGE_COTE, MARGE_HAUT + 10, options);
 
-        doc.addPage();
+    //     doc.addPage();
 
-        //ajout image
-        const img = new Image();
-        img.src = reponses.image1;
-        //doc.addImage(img, 'JPEG', 10, 78, 50, 15); //PROBLEME, NE RECONNAIS PAS QUE C'EST UNE IMAGE
+    //     //ajout image
+    //     const img = new Image();
+    //     img.src = reponses.image1;
+    //     //doc.addImage(img, 'JPEG', 10, 78, 50, 15); //PROBLEME, NE RECONNAIS PAS QUE C'EST UNE IMAGE
 
-        const number_of_pages = doc.internal.getNumberOfPages();
-        for (let i = 1; i <= number_of_pages; ++i) {
-            doc.setPage(i);
+    //     const number_of_pages = doc.internal.getNumberOfPages();
+    //     for (let i = 1; i <= number_of_pages; ++i) {
+    //         doc.setPage(i);
 
-            //header
-            doc.text(MARGE_COTE, MARGE_HAUT, "N° étudiant : 1 - N° sujet : 14582");
-            doc.text(LARGEUR_A4 - MARGE_COTE, MARGE_HAUT, "Sujet de Pierre Dupont", 'right');
+    //         //header
+    //         doc.text(MARGE_COTE, MARGE_HAUT, "N° étudiant : 1 - N° sujet : 14582");
+    //         doc.text(LARGEUR_A4 - MARGE_COTE, MARGE_HAUT, "Sujet de Pierre Dupont", 'right');
 
-            //footer
-            doc.setLineWidth(0.5);
-            doc.line(MARGE_COTE, HAUTEUR_A4 - MARGE_BAS - 5, LARGEUR_A4 - MARGE_COTE, HAUTEUR_A4 - MARGE_BAS - 5);
-            doc.text(MARGE_COTE, HAUTEUR_A4 - MARGE_BAS, "Pierre Carillo");
-            doc.text(LARGEUR_A4 / 2, HAUTEUR_A4 - MARGE_BAS, "IUT du Limousin - GMP", "center");
-            doc.text(LARGEUR_A4 - MARGE_COTE, HAUTEUR_A4 - MARGE_BAS, "Page " + i + "/" + number_of_pages, "right");
-        }
+    //         //footer
+    //         doc.setLineWidth(0.5);
+    //         doc.line(MARGE_COTE, HAUTEUR_A4 - MARGE_BAS - 5, LARGEUR_A4 - MARGE_COTE, HAUTEUR_A4 - MARGE_BAS - 5);
+    //         doc.text(MARGE_COTE, HAUTEUR_A4 - MARGE_BAS, "Pierre Carillo");
+    //         doc.text(LARGEUR_A4 / 2, HAUTEUR_A4 - MARGE_BAS, "IUT du Limousin - GMP", "center");
+    //         doc.text(LARGEUR_A4 - MARGE_COTE, HAUTEUR_A4 - MARGE_BAS, "Page " + i + "/" + number_of_pages, "right");
+    //     }
 
-        doc.save("sujet.pdf");
-    }
+    //     doc.save("sujet.pdf");
+    // }
 
     //affiche les différentes questions avec leurs réponses
     const displayQuestions = () => {
